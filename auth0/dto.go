99b52d1b50ac693d8e28daf2d863af35c65c9d09
Ibrahim Mbaziira <code.ibra@gmail.com>
2021-01-07 20:13:46 +0300

    add auth0 token generation functionality

---
+++ auth0/dto.go
@@ -0,0 +1,44 @@
+package auth0
+
+// AuthToken Struct
+type AuthToken struct {
+	Name  string `json:"name"`
+	Token string `json:"token"`
+}
+
+// Resp struct
+type Resp struct {
+	AccessToken string `json:"access_token"`
+	Scope       string `json:"scope"`
+	ExpiresIn   int    `json:"expires_in"`
+	TokenType   string `json:"token_type"`
+}
+
+// ESTokenSchema for AuthToken
+type ESTokenSchema struct {
+	Took     int  `json:"took"`
+	TimedOut bool `json:"timed_out"`
+	Shards   struct {
+		Total      int `json:"total"`
+		Successful int `json:"successful"`
+		Skipped    int `json:"skipped"`
+		Failed     int `json:"failed"`
+	} `json:"_shards"`
+	Hits struct {
+		Total struct {
+			Value    int    `json:"value"`
+			Relation string `json:"relation"`
+		} `json:"total"`
+		MaxScore float64 `json:"max_score"`
+		Hits     []struct {
+			Index  string  `json:"_index"`
+			Type   string  `json:"_type"`
+			ID     string  `json:"_id"`
+			Score  float64 `json:"_score"`
+			Source struct {
+				Name  string `json:"name"`
+				Token string `json:"token"`
+			} `json:"_source"`
+		} `json:"hits"`
+	} `json:"hits"`
+}
