b78721691b130c829a80e60693d1621b6e16897d
Ayman <enkhalifapro@gmail.com>
2021-01-14 18:54:21 +0200

    add affilliation get identity by user

--- affiliation/identity.go
+++ affiliation/identity.go
@@ -84,7 +84,7 @@ func (a *Affiliation) AddIdentity(identity *Identity) bool {
 	queryParams["email"] = identity.Email
 	queryParams["uuid"] = identity.UUID
 
-	endpoint := a.AffBaseURL + "/affiliation/" + url.PathEscape(a.ProjectSlug)+ "/add_identity/" + url.PathEscape(identity.Source)
+	endpoint := a.AffBaseURL + "/affiliation/" + url.PathEscape(a.ProjectSlug) + "/add_identity/" + url.PathEscape(identity.Source)
 	_, res, err := a.httpClient.Request(strings.TrimSpace(endpoint), "POST", headers, nil, queryParams)
 	if err != nil {
 		log.Println("Repository: AddIdentity: Could not insert the identity: ", err)
@@ -97,6 +97,81 @@ func (a *Affiliation) AddIdentity(identity *Identity) bool {
 		return false
 	}
 	return true
+}
+
+// GetIdentityByUser ...
+func (a *Affiliation) GetIdentityByUser(key string, value string) (*AffIdentity, error) {
+	if key == "" || value == "" {
+		nilKeyOrValueErr := "repository: GetIdentityByUser: key or value is null"
+		log.Println(nilKeyOrValueErr)
+		return nil, fmt.Errorf(nilKeyOrValueErr)
+	}
+	token, err := a.auth0Client.ValidateToken(a.Environment)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	headers := make(map[string]string, 0)
+	headers["Content-type"] = "application/json"
+	headers["Authorization"] = fmt.Sprintf("%s %s", "Bearer", token)
+
+	endpoint := a.AffBaseURL + "/affiliation/" + "identity/" + key + "/" + value
+	_, res, err := a.httpClient.Request(strings.TrimSpace(endpoint), "GET", headers, nil, nil)
+	if err != nil {
+		log.Println("Repository: GetIdentityByUser: Could not get the identity: ", err)
+		return nil, err
+	}
+
+	// Todo : need review
+	//var errMsg AffiliationsResponse
+	//err = json.Unmarshal(res, &errMsg)
+	//if err != nil || errMsg.Message != "" {
+	//	log.Println("Repository: GetIdentityByUser: failed to get identity: ", err)
+	//	return nil, err
+	//}
+
+	var ident IdentityData
+	err = json.Unmarshal(res, &ident)
+	if err != nil {
+		return nil, err
+	}
+
+	profileEndpoint := a.AffBaseURL + "/affiliation/" + url.PathEscape(a.ProjectSlug) + "/get_profile/" + *ident.UUID
+	_, profileRes, err := a.httpClient.Request(strings.TrimSpace(profileEndpoint), "GET", headers, nil, nil)
+	if err != nil {
+		log.Println("Repository: GetIdentityByUser: Could not get the identity: ", err)
+		return nil, err
+	}
+
+	var profile UniqueIdentityFullProfile
+	err = json.Unmarshal(profileRes, &profile)
+	if err != nil {
+		return nil, err
+	}
+	var identity AffIdentity
+	identity.UUID = ident.UUID
+	identity.Name = *ident.Name
+	identity.Username = *ident.Username
+	identity.Email = *ident.Email
+	identity.ID = &ident.ID
+
+	identity.IsBot = profile.Profile.IsBot
+	identity.Gender = profile.Profile.Gender
+	identity.GenderACC = profile.Profile.GenderAcc
+
+	if len(profile.Enrollments) > 1 {
+		identity.OrgName = &profile.Enrollments[0].Organization.Name
+		for _, org := range profile.Enrollments {
+			identity.MultiOrgNames = append(identity.MultiOrgNames, org.Organization.Name)
+		}
+	} else if len(profile.Enrollments) == 1 {
+		identity.OrgName = &profile.Enrollments[0].Organization.Name
+		identity.MultiOrgNames = append(identity.MultiOrgNames, profile.Enrollments[0].Organization.Name)
+	}
+
+	return &identity, nil
+
 }
 
 func buildServices(a *Affiliation) (httpClientProvider *http.ClientProvider, esClientProvider *elastic.ClientProvider, auth0ClientProvider *auth0.ClientProvider, err error) {
--- go.mod
+++ go.mod
@@ -5,9 +5,16 @@ go 1.15
 require (
 	github.com/avast/retry-go v3.0.0+incompatible
 	github.com/aws/aws-sdk-go v1.36.15
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/elastic/go-elasticsearch/v7 v7.10.0
+	github.com/go-git/go-git/v5 v5.2.0
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/google/uuid v1.1.4
 	github.com/json-iterator/go v1.1.10
 	github.com/stretchr/testify v1.6.1
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
+	golang.org/x/oauth2 v0.0.0-20201208152858-08078c50e5b5
 	golang.org/x/text v0.3.4
+	gopkg.in/yaml.v2 v2.2.8
 )
