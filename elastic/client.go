76755034ee149d4123b6d4ab6ee9a6a6837f61b6
linonymous <iamlinonymous@gmail.com>
2021-01-18 17:39:16 +0530

    Add bulk delete query

--- elastic/client.go
+++ elastic/client.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/elastic/go-elasticsearch/v7"
 	"log"
 	"regexp"
 	"strings"
@@ -246,6 +247,38 @@ func (p *ClientProvider) BulkInsert(data []BulkData) ([]byte, error) {
 		lines = append(lines, index)
 		lines = append(lines, "\n")
 		lines = append(lines, item.Data)
+		lines = append(lines, "\n")
+	}
+
+	body, err := json.Marshal(lines)
+	if err != nil {
+		return nil, errors.New("unable to convert body to json")
+	}
+
+	var re = regexp.MustCompile(`(}),"\\n",?`)
+	body = []byte(re.ReplaceAllString(strings.TrimSuffix(strings.TrimPrefix(string(body), "["), "]"), "$1\n"))
+
+	resData, err := p.Bulk(body)
+	if err != nil {
+		return nil, err
+	}
+
+	return resData, nil
+}
+
+// BulkDelete deletes more than one item using one request
+func (p *ClientProvider) BulkDelete(data []BulkData) ([]byte, error) {
+	lines := make([]interface{}, 0)
+
+	for _, item := range data {
+		indexName := item.IndexName
+		deleteQuery := map[string]interface{}{
+			"delete": map[string]string{
+				"_index": indexName,
+				"_id":    item.ID,
+			},
+		}
+		lines = append(lines, deleteQuery)
 		lines = append(lines, "\n")
 	}
 
