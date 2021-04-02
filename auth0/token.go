aa118e75676c9b6b1f166bfd0240fd70bca0389e
Ayman <enkhalifapro@yahoo.com>
2021-04-02 20:56:07 +0200

    DA-3612: code refactor

--- auth0/token.go
+++ auth0/token.go
@@ -95,7 +95,7 @@ func (a *ClientProvider) GetToken() (string, error) {
 			// refresh token before expiry by X minutes
 			if claims.VerifyExpiresAt(time.Now().Add(5*time.Minute).Unix(), false) == false {
 				if _, err := a.refreshToken(); err != nil {
-					log.Printf("Error refresh auth0 token %s", err.Error())
+					log.Printf("Error refresh auth0 token %s\n", err.Error())
 				}
 			}
 		}()
