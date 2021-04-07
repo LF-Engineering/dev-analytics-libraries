1e73eb2305d998e4e5fbc27e5ea2a185e4b9e24c
“khalifapro” <enkhalifapro@yahoo.com>
2021-04-07 16:26:18 +0200

    add configuration environment var

--- configuration/provider.go
+++ configuration/provider.go
@@ -1,32 +1,37 @@
-package configurations
+package configuration
 
 import (
 	"github.com/LF-Engineering/dev-analytics-libraries/aws/ssm"
+	"os"
 )
 
 // Configuration ...
 type Configuration struct {
-Ssm *ssm.SSM
+	Ssm *ssm.SSM
+	Env string
 }
 
 // NewClient ...
-func NewProvider() (*Configuration, error) {
+func NewProvider(env string) (*Configuration, error) {
 
 	s, err := ssm.NewSSMClient()
 	if err != nil {
 		return nil, err
 	}
 	config := new(Configuration)
-config.Ssm = s
+	config.Ssm = s
+	config.Env = env
 	return config, nil
 }
 
 // GetConfigValue ...
-func (c *Configuration) GetConfigValue(name string, decryption bool) (string, error){
+func (c *Configuration) GetConfigValue(name string, decryption bool) (string, error) {
+	if c.Env == "local" {
+		return os.Getenv(name), nil
+	}
 	param := c.Ssm.Param(name, decryption, false, "", "", "")
-
 	val, err := param.GetValue()
-	if err!= nil {
+	if err != nil {
 		return "", err
 	}
 	// val = os.Getenv(name)
@@ -34,13 +39,12 @@ func (c *Configuration) GetConfigValue(name string, decryption bool) (string, er
 	return val, nil
 }
 
-
 // SetConfigValue ...
-func (c *Configuration) SetConfigValue(name string, dataType string, paramType string, value string) (string, error){
+func (c *Configuration) SetConfigValue(name string, dataType string, paramType string, value string) (string, error) {
 	param := c.Ssm.Param(name, false, false, dataType, paramType, value)
 
 	val, err := param.SetValue()
-	if err!= nil {
+	if err != nil {
 		return "", err
 	}
 
@@ -48,11 +52,11 @@ func (c *Configuration) SetConfigValue(name string, dataType string, paramType s
 }
 
 // UpdateConfigValue ...
-func (c *Configuration) UpdateConfigValue(name string, dataType string, paramType string, value string, overwrite bool) (string, error){
+func (c *Configuration) UpdateConfigValue(name string, dataType string, paramType string, value string, overwrite bool) (string, error) {
 	param := c.Ssm.Param(name, false, overwrite, dataType, paramType, value)
 
 	val, err := param.UpdateValue()
-	if err!= nil {
+	if err != nil {
 		return "", err
 	}
 
