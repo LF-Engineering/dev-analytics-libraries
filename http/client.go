7b641825e4edd653e88d9ce9876a4055050d16d9
Bammeke Michael <mike_bammeke@yahoo.com>
2021-09-30 00:00:56 +0000

    add RequestWithHeaders function
    Signed-off-by: Bammeke Michael <mike_bammeke@yahoo.com>

--- http/client.go
+++ http/client.go
@@ -69,6 +69,47 @@ func (h *ClientProvider) Request(url string, method string, header map[string]st
 	return res.StatusCode, buf.Bytes(), nil
 }
 
+// RequestWithHeaders requests http and returns headers too
+func (h *ClientProvider) RequestWithHeaders(url string, method string, header map[string]string, body []byte, params map[string]string) (statusCode int, resBody []byte, resHeaders map[string][]string, err error) {
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(body))
+	if err != nil {
+		return 0, nil, nil, err
+	}
+
+	if cType, ok := header["Content-Type"]; !ok || cType == "application/json" {
+		req.Header.Add("Content-Type", "application/json")
+		delete(header, "Content-Type")
+	}
+
+	if header != nil {
+		for k, v := range header {
+			req.Header.Add(k, v)
+		}
+	}
+
+	if params != nil {
+		q := req.URL.Query()
+		for k, v := range params {
+			q.Add(k, v)
+		}
+		req.URL.RawQuery = q.Encode()
+	}
+
+	// Do request
+	res, err := h.httpclient.Do(req)
+	if err != nil {
+		return 0, nil, nil, err
+	}
+
+	var buf bytes.Buffer
+	_, err = buf.ReadFrom(res.Body)
+	if err != nil {
+		return 0, nil, nil, err
+	}
+
+	return res.StatusCode, buf.Bytes(), res.Header, nil
+}
+
 // RequestCSV requests http API that returns csv result
 func (h *ClientProvider) RequestCSV(url string) ([][]string, error) {
 	resp, err := http.Get(url)
